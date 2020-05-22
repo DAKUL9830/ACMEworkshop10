39a844836e7c104e6f0192abd8ddc0ab37d3f625
DAKUL9830 <dimaakulau1988@gmail.com>
2020-05-21 21:07:43 -0400

    updated

--- apps.js
+++ apps.js
@@ -3,22 +3,22 @@ const companies_API = 'https://acme-users-api-rev.herokuapp.com/api/companies';
 const root = document.querySelector('#root');
 
 class Counter extends React.Component{
-    state={
+    /*state={
         prodCount:0,
         compCount:0,
 
-    }
+    }*/
 
         
     
     render(){
-     const {compCount}=this.props;
-     const{prodCount}=this.props;
+     const {companies}=this.props;
+     const{products}=this.props;
      return React.createElement(
 
          'span',                               //COUNTING THE AMOUNT OF COMPANIES AND PRODUCTS
          null,
-         `Acme -we have ${prodCount} Products and ${compCount} Companies`
+         `Acme -we have ${products.length} Products and ${companies.length} Companies`
 
      )
     }
@@ -26,16 +26,68 @@ class Counter extends React.Component{
   }
 
 class ProductList extends React.Component {
-    state = {
-           
-            products: [],                                 //CREATING PRODUCT LIST
-        }
+   
 
+  
+    
+
+    render () {
+        const prodMap =  this.props.state.products.map(elem => {
+           return React.createElement('li', null, elem);
+        
+    })
+        return React.createElement('ul', null, ...prodMap)
+}
+}
+
+class CompanyList extends React.Component {
+ 
+                                               //CREATING COMPANY LIST
+
+
+
+render () {
+    const compMap =  this.props.state.companies.map(elem => {
+       return React.createElement('li', null, elem);
+    
+})
+    return React.createElement('ul', null, ...compMap)
+}
+}
+
+
+
+
+class List extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            companies: [],
+            products: [],
+           // compCount:0,
+           // prodCount:0,
+           
+        };
+        
+
+    }
     componentDidMount() {
-        fetch(products_API)
-        .then(product => {
-            return product.json();
+        fetch(companies_API)
+        .then(company => {
+            return company.json();
         }).then((data) => {
+            console.log(data);
+            let compArr = [];
+            data.map(comp => {
+            compArr.push(comp.name);
+             
+           })
+           this.setState({ companies: compArr});
+            })
+         fetch(products_API)
+         .then(product => {
+            return product.json();
+           }).then((data) => {
             console.log(data);
             let prodArr = [];
             data.map(prod => {
@@ -45,73 +97,12 @@ class ProductList extends React.Component {
            this.setState({ products: prodArr});
             })
         }
-    
-
-    render () {
-        const prodMap =  this.state.products.map(elem => {
-           return React.createElement('li', null, elem);
-        
-    })
-        return React.createElement('ul', null, ...prodMap)
-}
-}
-
-class CompanyList extends React.Component {
-    state = {
-         companies: [],
-         
-     }
-                                               //CREATING COMPANY LIST
-componentDidMount() {
-    fetch(companies_API)
-    .then(company => {
-        return company.json();
-    }).then((data) => {
-        console.log(data);
-        let compArr = [];
-        data.map(comp => {
-        compArr.push(comp.name);
-         
-       })
-       this.setState({ companies: compArr});
-        })
-    }
-
-
-render () {
-    const compMap =  this.state.companies.map(elem => {
-       return React.createElement('li', null, elem);
-    
-})
-    return React.createElement('ul', null, ...compMap)
-}
-}
-
-// class Nav extends React.Component {
-
-// }
-
-
-class List extends React.Component {
-    constructor () {
-        super();
-        this.state = {
-            companies: [],
-            products: [],
-            compCount:0,
-            prodCount:0,
-           
-        };
-        /*this.incrementProd=this.incrementProd.bind(this);
-        this.incrementComp=this.incrementComp.bind(this)*/
-
-    }
-    setCountComp(compCount){
+   /* setCountComp(compCount){
         this.setState({compCount})
     }
     setCountProd(prodCount){
         this.setState({prodCount})
-    }
+    }*/
      /*incrementProd(){
          const {prodCount}=this.state;
          this.setState({
@@ -127,7 +118,8 @@ class List extends React.Component {
     }*/
 
 render(){
-    const { companies, products ,compCount,prodCount} = this.state;
+    const { companies, products } = this.state;
+
     // return React.createElement (
     //     'ul',
     //     null,
@@ -144,7 +136,7 @@ render(){
     // if (view === 'products') {
     //     chosenView = React.createElement(ProductList, { products });
     // }
-    return React.createElement('div', null, React.createElement(Counter,{prodCount,compCount,}),React.createElement(ProductList),React.createElement(CompanyList));
+    return React.createElement('div', null, React.createElement(Counter,{products,companies,}),React.createElement(ProductList,{products}),React.createElement(CompanyList,{companies}));
    // return React.createElement('div', null, React.createElement(ProductList));
 }
 }
